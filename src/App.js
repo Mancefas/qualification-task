a9027c7f0ae55458aefdc2f35e974ec8e8953b2e
Mancefas <mantvydasliobe@gmail.com>
2021-11-21 20:02:31 +0200

    lazy load NewRecordFormPage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, Suspense } from "react";
+
+import { CircularProgress, Box } from "@mui/material";
 
 import { Route, Switch } from "react-router-dom";
 
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import NewRecordFormPage from "./Pages/NewRecordFormPage";
+
 import ListPage from "./Components/ListPage";
 import DetailsPage from "./Components/DetailsPage";
 
 import Context from "./store/Context";
+
+const NewRecordFormPage = React.lazy(() => import("./Pages/NewRecordFormPage"));
 
 function App() {
   const context = useContext(Context);
@@ -18,9 +22,17 @@ function App() {
       <Header />
       <Switch>
         <section>
-          <Route path="/form-page" exact>
-            <NewRecordFormPage />
-          </Route>
+          <Suspense
+            fallback={
+              <Box sx={{ textAlign: "center", minHeight: "70vh" }}>
+                <CircularProgress />
+              </Box>
+            }
+          >
+            <Route path="/form-page" exact>
+              <NewRecordFormPage />
+            </Route>
+          </Suspense>
           <Route path="/">{context.showListPage && <ListPage />}</Route>
         </section>
       </Switch>
